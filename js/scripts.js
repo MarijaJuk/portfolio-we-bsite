43ec03309f82fdc7b03bbdedf85527add688139d
MarijaJuk <97982683+MarijaJuk@users.noreply.github.com>
2022-02-15 19:43:38 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hey, welcome to my Portfolio Site!")
+console.log('Hey, welcome to my Portfolio Site!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
